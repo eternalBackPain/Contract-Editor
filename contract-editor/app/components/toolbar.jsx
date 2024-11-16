3f8c042df1e15bb7f37afdc224528013f6f395ca
eternalBackPain <johnnaoom@gmail.com>
2024-11-16 15:10:43 +1100

    styling

---
+++ contract-editor/app/components/toolbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ToolBar = () => {
+  return (
+    <div className="bg-gray-300 w-full h-12 mt-2">
+      <p>File</p>
+      <p>Edit</p>
+      <p>Tools</p>
+      <p>Export</p>
+    </div>
+  );
+};
+
+export default ToolBar;
--- contract-editor/app/layout.js
+++ contract-editor/app/layout.js
@@ -1,15 +1,24 @@
+import NavBar from "./components/nav";
+import ToolBar from "./components/toolbar";
 import "./globals.css";
 
 export const metadata = {
   title: "Contract Editor",
   description: "Drafting contracts programmatically",
+  keywords: "contract editor, lawyers, law, contracts",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
+      {/* body sets the container */}
+      <body className="bg-background text-foreground font-sans antialiased mb-40 mx-4 mt-2">
+        {/* main sets the alignment */}
+        <main className="flex flex-col">
+          <NavBar />
+          <ToolBar />
+          {children}
+        </main>
       </body>
     </html>
   );
